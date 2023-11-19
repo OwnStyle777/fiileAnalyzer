d2272435ccee31bddfb29dfdd5ab04841ccac82a
OwnStyle777 <OwnStyleXD@gmail.com>
2023-11-19 12:47:14 +0100

    refactoring of Main and FileProcessor classes

--- FileProcessor.java
+++ FileProcessor.java
@@ -1,8 +1,39 @@
 package analyzer;
 
-public class FileProcessor implements Runnable {
-    @Override
-    public void run() {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
+public class FileProcessor  {
+
+    public void processFiles(List<File> listOfFiles, String fileType, String filePattern) {
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+        for (File file : listOfFiles) {
+            executor.submit(() -> {
+                try (InputStream inputStream = new FileInputStream(file)) {
+                    byte[] pdfBytes = filePattern.getBytes();
+                    byte[] allBytes = inputStream.readAllBytes();
+
+                    if (SearchingMethods.searchPatternKMP(allBytes, pdfBytes)) {
+                        System.out.println(file.getName() + ": " + fileType);
+                    } else {
+                        System.out.println(file.getName() + ": Unknown file type");
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        executor.shutdown();
+        try {
+            executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
--- Main.java
+++ Main.java
@@ -5,39 +5,19 @@ import java.io.*;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
-
-        File pdfFile = new File(args[0]);
+        File pdfFile = new File( args[0]);
         List<File> listOfFiles = Arrays.asList(Objects.requireNonNull(pdfFile.listFiles()));
-        String pdfPattern = args[1];
+        String filePattern = args[1];
+        String fileType = args[2];
 
-
-
-        for (File file : listOfFiles) {
-            long startTime = System.currentTimeMillis();
-            try (InputStream inputStream = new FileInputStream(file)) {
-                byte[] pdfBytes = pdfPattern.getBytes();
-                byte[] allBytes = inputStream.readAllBytes();
-
-
-                if (SearchingMethods.searchPatternKMP(allBytes, pdfBytes)) {
-//                    System.out.println(args[2]);
-                    System.out.println(file.getName() + ": " + args[2]);
-//                    long elapsedTime = System.currentTimeMillis() - startTime;
-//                    System.out.println("It took " + (double) elapsedTime / 10000 + "seconds");
-
-                } else {
-                    System.out.println(file.getName() + ": Unknown file type");
-//                    long elapsedTime = System.currentTimeMillis() - startTime;
-//                    System.out.println("It took " + (double) elapsedTime / 10000 + "seconds");
-
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+        FileProcessor fileProcessor = new FileProcessor();
+        fileProcessor.processFiles(listOfFiles, fileType, filePattern);
     }
 }
 
